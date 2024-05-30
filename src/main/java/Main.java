43e4baf3d844dc2cc462e7a1747393e9a438b477
Vegadelalyra <lyradeperseo@gmail.com>
2024-05-29 20:23:43 -0500

    Learning more on IntelliJ

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, Vega de la Lyra!");
+        Person person = new Person("Jamila");
+        System.out.println(person.getName());
+    }
+}
